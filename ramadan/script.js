f79ba8b3d346cc5fb9dbc43939ed967ba324bac4
drahmedgabr <ahmadal4ever@gmail.com>
2024-03-09 12:01:15 +0200

    add marayam website

--- ramadan/script.js
+++ ramadan/script.js
@@ -1,6 +1,20 @@
 const ramadanSong = new Audio("https://naghma.naghma.mobi/uploads/songs/Ramadan/Wa7wy_Ya_Wa7wy.mp3");
-const body = document.body;
+const playBtn = document.getElementById("playBtn");
+var playing = false;
 
-body.addEventListener('mouseover', () => {
-    ramadanSong.play();
-});
+function playSong() {
+    if(playing == false){
+        switchIcon();
+        playing = true;
+        ramadanSong.play();
+    } else {
+        switchIcon();
+        playing = false;
+        ramadanSong.pause();
+    }
+}
+
+function switchIcon() {
+    playBtn.classList.toggle("bi-play-circle-fill");
+    playBtn.classList.toggle("bi-pause-circle-fill");
+}
